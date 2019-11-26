a1199fc18be4e636d58c2700cdb498b75ce55862
nreddystudent <nreddy@student.wethinkcode.co.za>
2019-11-26 12:39:55 +0200

    finished mvc module

--- models/product.js
+++ models/product.js
@@ -1,31 +1,32 @@
 const products = [];
 const fs = require('fs');
 const path = require('path');
-module.exports = class Product{
-	constructor(t){
+
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+const getProductsFromFile = (cb) => {
+	fs.readFile(p, (err, data) => {
+		if (err) {
+			return cb([]);
+		}
+		else {
+			cb(JSON.parse(data));
+		}
+	})
+}
+module.exports = class Product {
+	constructor(t) {
 		this.title = t;
-	}	
-	save(){
+	}
+	save() {
 		const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-		fs.readFile(p, (err, data) =>{
-			let products = [];
-			if (!err){
-				products = JSON.parse(data);
-			}
-			console.log(data);
+		getProductsFromFile((products) => {
 			products.push(this);
-			fs.writeFile(p, JSON.stringify(products), (err)=>{
-				console.log(err); 
+			fs.writeFile(p, JSON.stringify(products), (err) => {
+				console.log(err);
 			});
 		})
 	}
-	static fetchAll(cb){
-		const p = path.join(path.dirname(process.mainModule.filename), 'data',  'products.json');
-		fs.readFile(p, (err, data) =>{
-			if (err){
-				cb([]);
-			}
-			cb (JSON.parse(data))
-		})
+	static fetchAll(cb) {
+		getProductsFromFile(cb);
 	}
 }
